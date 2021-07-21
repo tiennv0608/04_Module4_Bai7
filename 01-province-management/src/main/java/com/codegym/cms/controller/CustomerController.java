82f213c6b5739c5e3c65f5fefb87cb99a7ae3216
tiennv0608 <tiennv0608@gmail.com>
2021-07-21 14:21:04 +0700

    b1

---
+++ 01-province-management/src/main/java/com/codegym/cms/controller/CustomerController.java
@@ -0,0 +1,110 @@
+package com.codegym.cms.controller;
+
+import com.codegym.cms.model.Customer;
+import com.codegym.cms.model.Province;
+import com.codegym.cms.service.ICustomerService;
+import com.codegym.cms.service.IProvinceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class CustomerController {
+
+    @Autowired
+    private ICustomerService customerService;
+
+    @Autowired
+    private IProvinceService provinceService;
+
+    @ModelAttribute("provinces")
+    public Iterable<Province> provinces() {
+        return provinceService.findAll();
+    }
+
+    @GetMapping("/customers")
+    public ModelAndView listCustomers() {
+        Iterable<Customer> customers = customerService.findAll();
+        ModelAndView modelAndView = new ModelAndView("/customer/list");
+        modelAndView.addObject("customers", customers);
+        return modelAndView;
+    }
+
+    @GetMapping("/create-customer")
+    public ModelAndView showCreateForm() {
+        ModelAndView modelAndView = new ModelAndView("/customer/create");
+        modelAndView.addObject("customer", new Customer());
+        return modelAndView;
+    }
+
+    @PostMapping("/create-customer")
+    public ModelAndView saveCustomer(@ModelAttribute("customer") Customer customer) {
+        customerService.save(customer);
+        ModelAndView modelAndView = new ModelAndView("/customer/create");
+        modelAndView.addObject("customer", new Customer());
+        modelAndView.addObject("message", "New customer created successfully");
+        return modelAndView;
+    }
+
+    @GetMapping("/edit-customer/{id}")
+    public ModelAndView showEditForm(@PathVariable Long id) {
+        Optional<Customer> customer = customerService.findById(id);
+        if (customer.isPresent()) {
+            ModelAndView modelAndView = new ModelAndView("/customer/edit");
+            modelAndView.addObject("customer", customer);
+            return modelAndView;
+        } else {
+            ModelAndView modelAndView = new ModelAndView("/error.404");
+            return modelAndView;
+        }
+    }
+
+    @PostMapping("/edit-customer")
+    public ModelAndView updateCustomer(@ModelAttribute("customer") Customer customer) {
+        customerService.save(customer);
+        ModelAndView modelAndView = new ModelAndView("/customer/edit");
+        modelAndView.addObject("customer", customer);
+        modelAndView.addObject("message", "Customer updated successfully");
+        return modelAndView;
+    }
+
+    @GetMapping("/delete-customer/{id}")
+    public ModelAndView showDeleteForm(@PathVariable Long id) {
+        Optional<Customer> customer = customerService.findById(id);
+        if (customer.isPresent()) {
+            ModelAndView modelAndView = new ModelAndView("/customer/delete");
+            modelAndView.addObject("customer", customer);
+            return modelAndView;
+        } else {
+            ModelAndView modelAndView = new ModelAndView("/error.404");
+            return modelAndView;
+        }
+    }
+
+    @PostMapping("/delete-customer")
+    public String deleteCustomer(@ModelAttribute("customer") Customer customer) {
+        customerService.remove(customer.getId());
+        return "redirect:customers";
+    }
+
+    @GetMapping("/view-province/{id}")
+    public ModelAndView viewProvince(@PathVariable("id") Long id) {
+        Optional<Province> provinceOptional = provinceService.findById(id);
+        if (!provinceOptional.isPresent()) {
+            return new ModelAndView("/error.404");
+        }
+        Iterable<Customer> customers = customerService.findAllByProvince(provinceOptional.get());
+        ModelAndView modelAndView = new ModelAndView("/province/view");
+        modelAndView.addObject("province", provinceOptional.get());
+        modelAndView.addObject("customers", customers);
+        return modelAndView;
+    }
+
+}
---
+++ 01-province-management/src/main/java/com/codegym/cms/repository/IProvinceRepository.java
@@ -0,0 +1,7 @@
+package com.codegym.cms.repository;
+
+import com.codegym.cms.model.Province;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface IProvinceRepository extends PagingAndSortingRepository<Province, Long> {
+}
